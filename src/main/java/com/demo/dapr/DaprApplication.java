9633aa10f4d8c7efa86d559affbf980a6932d3dd
chandra <chandra.mallu@imaginelearning.com>
2024-03-13 17:31:32 -0400

    Intial commit

---
+++ src/main/java/com/demo/dapr/DaprApplication.java
@@ -0,0 +1,16 @@
+package com.demo.dapr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaprApplication.class, args);
+
+
+
+	}
+
+}
---
+++ src/test/java/com/demo/dapr/DaprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.dapr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
